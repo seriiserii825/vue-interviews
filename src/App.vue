df825ab42d346ab68a4ff00db63d5609eaff624b
serii <seriiburduja@gmail.com>
2024-05-20 13:43:08 +0300

    feat: app-header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
+import AppHeader from '@/components/AppHeader.vue'
 </script>
-
 <template>
-  <RouterView />
+  <div class="container m-auto">
+    <AppHeader />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {RouterLink} from 'vue-router'
+import {ref} from 'vue'
+const items = ref([
+  {
+    'label': 'Authorization',
+    'icon': 'pi pi-user',
+    'path': '/auth'
+  },
+  {
+    'label': 'Add',
+    'icon': 'pi pi-plus',
+    'path': '/'
+  },
+  {
+    'label': 'Interview list',
+    'icon': 'pi pi-list',
+    'path': '/list'
+  },
+  {
+    'label': 'Statistics',
+    'icon': 'pi pi-chart-pie',
+    'path': '/statistics'
+  },
+])
+</script>
+<template>
+  <app-menubar :model="items" class="menu">
+    <template #item="{item, props}">
+      <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
+        <span :class="item.icon" class="p-menuitem-icon"></span>
+        <span class="ml-2">{{ item.label }}</span>
+      </router-link>
+    </template>
+  </app-menubar>
+</template>
+<style scoped>
+.menu {
+  margin: 30px 0;
+}
+</style>

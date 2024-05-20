1ff204fa06f99af71cc6b83bcc9e90a890ec30c5
serii <seriiburduja@gmail.com>
2024-05-20 13:26:59 +0300

    feat: istall primevuew

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import './assets/main.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevueicons/primevueicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +12,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue)
 app.use(router)
 
 app.mount('#app')

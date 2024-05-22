d29ca0bbaec4a053448d14557c8f621bfddf20d3
serii <seriiburduja@gmail.com>
2024-05-22 17:27:41 +0300

    feat: login register

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,41 @@
-import './assets/main.css'
-import 'primevue/resources/themes/saga-blue/theme.css'
-import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css'
+import {initializeApp} from "firebase/app";
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import './assets/main.css';
+import ToastService from "primevue/toastservice";
+import Toast from "primevue/toast";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config'
-import Menubar from 'primevue/menubar'
+import {createPinia} from 'pinia';
+import PrimeVue from 'primevue/config';
+import Menubar from 'primevue/menubar';
+import {createApp} from 'vue';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzM0gZXVW_6i5fjPSL9wkYeYBUi7CjnJY",
+  authDomain: "vue-interviews-2e421.firebaseapp.com",
+  projectId: "vue-interviews-2e421",
+  storageBucket: "vue-interviews-2e421.appspot.com",
+  messagingSenderId: "79925594363",
+  appId: "1:79925594363:web:8795d44b10da590bbd485e"
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
 
+app.use(ToastService)
 app.use(createPinia())
 app.use(PrimeVue)
 app.use(router)
 app.component('app-menubar', Menubar)
+app.component('app-input-text', InputText)
+app.component('app-button', Button)
+app.component('app-toast', Toast)
 
 app.mount('#app')
--- src/views/PageAuth.vue
+++ src/views/PageAuth.vue
@@ -1,11 +1,73 @@
 <script setup lang="ts">
 import {useUserStore} from '@/stores/user-store';
 import {storeToRefs} from 'pinia';
-import {onMounted} from 'vue'
+import {computed, onMounted, ref} from 'vue'
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
+import {useToast} from 'primevue/usetoast'
 import {useRouter} from 'vue-router';
 const router = useRouter()
 const userStore = useUserStore()
 const {user_id} = storeToRefs(userStore)
+const toast = useToast()
+
+const is_login = ref(true)
+const email = ref('')
+const password = ref('')
+const isLoading = ref(false)
+
+const subtitleText = computed(() => is_login.value ? 'Login' : 'Register')
+const linkAccountText = computed(() => is_login.value ? 'Register' : 'Login')
+const submitButtonText = computed(() => is_login.value ? 'Login' : 'Register')
+
+function toggleAuth() {
+  is_login.value = !is_login.value
+}
+
+function submitForm() {
+  if (is_login.value) {
+    signIn()
+  } else {
+    signUp()
+  }
+}
+
+async function signIn() {
+  const auth = getAuth()
+  try {
+    isLoading.value = true
+    const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value)
+    const user = userCredential.user
+    console.log(user)
+    isLoading.value = false
+    router.push('/')
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      toast.add({severity: 'error', summary: 'Error', detail: error.message})
+    }
+    isLoading.value = false
+  }finally {
+    isLoading.value = false
+  }
+}
+
+async function signUp() {
+  const auth = getAuth()
+  try {
+    isLoading.value = true
+    const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value)
+    const user = userCredential.user
+    console.log(user)
+    isLoading.value = false
+    router.push('/')
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      toast.add({severity: 'error', summary: 'Error', detail: error.message})
+    }
+    isLoading.value = false
+  }finally {
+    isLoading.value = false
+  }
+}
 
 onMounted(() => {
   // user_id.value = 'user'
@@ -13,5 +75,27 @@ onMounted(() => {
 </script>
 
 <template>
-  <h2>Auth page</h2>
+  <app-toast position="top-right" />
+  <div class="flex justify-content-center p-2">
+    <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+      <div class="text-center mb-3">
+        <div class="text-900 text-3xl font-medium mb-3">Welcome</div>
+        <span class="text-600 font-medium line-height-3">{{ subtitleText }}</span>
+        <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer" @click="toggleAuth">
+          {{ linkAccountText }}
+        </a>
+      </div>
+
+      <form @submit.prevent="submitForm">
+        <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+        <app-input-text v-model="email" id="email1" type="email" class="w-full mb-3" />
+
+        <label for="password1" class="block text-900 font-medium mb-2">Пароль</label>
+        <app-input-text v-model="password" id="password1" type="password" class="w-full mb-3" />
+
+        <app-button :label="submitButtonText" type="submit" icon="pi pi-user" :loading="isLoading"
+          class="w-full"></app-button>
+      </form>
+    </div>
+  </div>
 </template>
